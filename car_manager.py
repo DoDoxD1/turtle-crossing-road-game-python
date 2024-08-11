bfee4d30917ea8dd7f229d48286796449a3cd03a
= <=>
2024-08-11 18:53:55 +0530

    create cars and detect collision

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,38 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+RANDOM_Y = [-200, -150, -100, -50, 0, 50, 100, 150, 200]
+SHAPE = "square"
+HEADING = 180
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.cars = []
+        self.hideturtle()
+        self.car_frequency = 10
+
+    def make_car(self):
+        if random.randint(0, self.car_frequency) == 1:
+            car = Turtle()
+            car.shape(SHAPE)
+            car.shapesize(stretch_wid=1, stretch_len=2)
+            car.penup()
+            car.setheading(HEADING)
+            car.color(random.choice(COLORS))
+            car.goto(290, random.choice(RANDOM_Y))
+            self.cars.append(car)
+
+    def move(self):
+        for car in self.cars:
+            car.forward(STARTING_MOVE_DISTANCE)
+
+    def detect_collision(self, player):
+        for car in self.cars:
+            if car.distance(player) < 20:
+                print("collision")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from turtle import Screen
 from player import Player
@@ -16,10 +17,18 @@ player = Player()
 # screen listen on keypress
 screen.listen()
 screen.onkey(key="w", fun=player.move)
+screen.onkey(key="Up", fun=player.move)
 
+# set up the cars
+car_manager = CarManager()
 
 # checking if game is still on
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.make_car()
+    car_manager.move()
+    car_manager.detect_collision(player)
+
+screen.exitonclick()
