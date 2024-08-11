35bd3ac3e5c066f5ec2e30703df892a28d810734
= <=>
2024-08-11 18:00:02 +0530

    move the turtle with keypress

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+# screen setup
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Turtle Crossing the Road")
 screen.tracer(0)
 
+# set up the player turtle
+player = Player()
+
+# screen listen on keypress
+screen.listen()
+screen.onkey(key="w", fun=player.move)
+
+
+# checking if game is still on
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
